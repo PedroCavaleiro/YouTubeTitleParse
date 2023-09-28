9a8fcd3a03b84e15d4286290b3022ec88b6a7014
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2023-09-28 21:00:13 +0100

    Allows being used as extension

--- lib/Parse.cs
+++ lib/Parse.cs
@@ -8,7 +8,7 @@ public static class Parse {
     /// <param name="text">The title to process</param>
     /// <param name="options">Options defining which plugins to run and the order. When null the default will be loaded</param>
     /// <returns>Object with the Artist (can be null) and Title</returns>
-    public static ArtistTitle GetArtistTitle(string text, Options options = null) {
+    public static ArtistTitle GetArtistTitle(this string text, Options options = null) {
         var loadedOptions = new Options();
         loadedOptions.LoadDefaults();
         if (options != null)
